ab6943aa6dbcaae7dc920ffc6088f9c8e49b8ac6
Sofyan Egi Lesmana <sofyanegil@gmail.com>
2022-05-14 11:50:58 +0700

    Init Composable

---
+++ app/src/main/java/id/ac/unpas/launcherapp/MainScreen.kt
@@ -0,0 +1,18 @@
+package id.ac.unpas.launcherapp
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import id.ac.unpas.launcherapp.ui.theme.LauncherAppTheme
+
+@Composable
+fun MainScreen() {
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LauncherAppTheme {
+        MainScreen()
+    }
+}
