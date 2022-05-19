ae593e621088e005b9c8623ab90b960bf430ee8d
Sofyan Egi Lesmana <sofyanegil@gmail.com>
2022-05-19 11:54:53 +0700

    Use suspend Function

--- app/src/main/java/id/ac/unpas/launcherapp/MainScreen.kt
+++ app/src/main/java/id/ac/unpas/launcherapp/MainScreen.kt
@@ -1,13 +1,39 @@
 package id.ac.unpas.launcherapp
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import id.ac.unpas.launcherapp.ui.theme.LauncherAppTheme
+import kotlinx.coroutines.launch
 
 @Composable
-fun MainScreen() {
+fun MainScreen(scaffoldState: ScaffoldState = rememberScaffoldState()) {
+    val scope = rememberCoroutineScope()
+
+    Scaffold(scaffoldState = scaffoldState) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+
+        ) {
+            Button(onClick = {
+                scope.launch {
+                    scaffoldState.snackbarHostState.showSnackbar("Hello, button diklik")
+                }
+            }) {
+                Text(text = "Klik Saya")
+            }
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
